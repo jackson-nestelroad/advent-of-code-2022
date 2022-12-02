fa909ced4cc6697f9212a046db88c80eea4d8d12
Jackson Nestelroad <jackson@nestelroad.com>
2022-12-01 18:07:06 -0800

    Rename lines.rs to blocks.rs

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,7 +1,7 @@
+mod blocks;
 mod error;
-mod lines;
 mod solver;
 
+pub use blocks::{NewlineBlocks, NewlineBlocksIterator};
 pub use error::{AocError, AocResult, IntoAocResult};
-pub use lines::{NewlineBlocks, NewlineBlocksIterator};
 pub use solver::SolverFn;
